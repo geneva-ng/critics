1f791d2be414a77fcdc65d38be62be9bc28e76e0
Geneva Ng <geneva@instalily.ai>
2024-12-21 01:25:05 -0500

    finalized the json schema

---
+++ functions.py
@@ -0,0 +1,4 @@
+"""
+Show your schema.json file to ChatGPT and ask it to get some basic update/delete/add functions. 
+Test in python locally, then convert to TypeScript/Node.js to be able to run on Firebase.
+"""
